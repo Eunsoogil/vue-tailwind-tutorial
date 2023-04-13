61bbe08d0097ea4e79b7c5ce3d7a209258eebdb3
길은수 <humanity1st@naver.com>
2023-04-13 17:27:26 +0900

    tailwind setup

--- vue-spa-tutorial/src/main.js
+++ vue-spa-tutorial/src/main.js
@@ -1,6 +1,7 @@
 import {createApp, VueElement} from 'vue'
 import App from './App.vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+// import './assets/output.css'
 import $bus from './utils/event'
 import router from './router'
 import $pages from './data'
@@ -8,9 +9,6 @@ import $pages from './data'
 const app = createApp(App)
 
 app.use(router)
-
-// app.config.globalProperties.$bus = $bus
-// app.config.globalProperties.$pages = $pages
 
 app.provide('$bus', $bus)
 app.provide('$pages', $pages)
